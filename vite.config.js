3de27f874d2dceb042cfed5a16c0db89e7f49dc8
Mehak Choudhary <choudharymehakk@gmail.com>
2026-04-03 19:56:02 +0530

    fix vite allowed host

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 10000,
+    allowedHosts: ['uni-rent-ws7y.onrender.com/']
+  }
 })
